6545ac52058f527ea0119d34e96efb38906957cd
Mason Matlock <mwilliam09@gmail.com>
2016-03-11 18:32:49 -0600

    Initial Commit: Working maze generation, Roughed out gameObjs. To-Do: Movement.

---
+++ src/BrainEaters/wwwroot/js/BrainEaters.js
@@ -0,0 +1,106 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+window.onload = function () {
+    var spriteSize = 32;
+    var canvas = document.getElementById("gameSpace");
+    // ReSharper disable once AssignedValueIsNeverUsed
+    var context = canvas.getContext("2d");
+    var canvasWidth = canvas.offsetWidth;
+    var canvasHeight = canvas.offsetHeight;
+    var numCol = Math.floor(canvasWidth / spriteSize); //20
+    var numRow = Math.floor(canvasHeight / spriteSize); //15
+    //console.log(numCol);
+    //console.log(numRow);
+    var gameArr = []; //why const?
+    for (var i = 0; i < numRow; i++) {
+        gameArr[i] = [];
+    }
+    var GameObj = (function () {
+        function GameObj(posRow, posCol, isSolid, src) {
+            var _this = this;
+            if (src === void 0) { src = "../images/bg_tile.png"; }
+            this.posRow = posRow;
+            this.posCol = posCol;
+            this.isSolid = isSolid;
+            this.gameObjImg = new Image();
+            this.gameObjImg.onload = function () {
+                context.drawImage(_this.gameObjImg, posCol * spriteSize, posRow * spriteSize, spriteSize, spriteSize);
+            };
+            this.gameObjImg.src = src;
+        }
+        return GameObj;
+    }());
+    var Player = (function (_super) {
+        __extends(Player, _super);
+        function Player() {
+            _super.call(this, 1, (Math.floor(Math.random() * (numCol - 2) + 1)), true, "../images/plSprite.png");
+        }
+        Player.prototype.mov = function () { };
+        return Player;
+    }(GameObj));
+    var Zombie = (function (_super) {
+        __extends(Zombie, _super);
+        function Zombie(varRow) {
+            _super.call(this, varRow, (Math.floor(Math.random() * (numCol - 2) + 1)), true, "../images/zomSprite.png");
+        }
+        Zombie.prototype.mov = function () {
+            // ReSharper disable TsNotResolved
+            if (true /*changelater*/) {
+                this.posCol = Math.floor(Math.random() + 1);
+            }
+            this.posRow = Math.floor(Math.random() + 1);
+            // ReSharper restore TsNotResolved
+        };
+        return Zombie;
+    }(GameObj));
+    populateGame();
+    console.log(gameArr);
+    // ReSharper disable VariableCanBeMadeConst
+    // ReSharper disable UnusedLocals
+    var player = new Player();
+    var zomRow;
+    do {
+        zomRow = Math.floor(Math.random() * (numRow - 6) + 5);
+    } while (zomRow % 2 === 0);
+    var zombie0 = new Zombie(zomRow);
+    do {
+        zomRow = Math.floor(Math.random() * (numRow - 6) + 5);
+    } while (zomRow % 2 === 0);
+    var zombie1 = new Zombie(zomRow);
+    // ReSharper restore UnusedLocals
+    // ReSharper restore VariableCanBeMadeConst
+    function populateGame() {
+        for (var row = 0; row < numRow; row++) {
+            var numEmptyMax = Math.floor(Math.random() * 2 + 4);
+            //console.log(numEmptyMax);
+            // ReSharper disable once VariableCanBeMovedToInnerBlock
+            var numEmpty = 0; //why move inside?
+            for (var col = 0; col < numCol; col++) {
+                //gameArr[row][col] = 0;
+                if (row === 0 || row === numRow - 1) {
+                    gameArr[row][col] = new GameObj(row, col, true);
+                }
+                else if (col === 0 || col === numCol - 1) {
+                    gameArr[row][col] = new GameObj(row, col, true);
+                }
+                else if (row % 2 !== 0) {
+                    gameArr[row][col] = -9;
+                }
+                else {
+                    var coin = Math.floor(Math.random() * 10);
+                    if (coin <= 2 && numEmpty < numEmptyMax) {
+                        gameArr[row][col] = -9;
+                        ++numEmpty;
+                    }
+                    else {
+                        gameArr[row][col] = new GameObj(row, col, true);
+                    }
+                }
+            }
+        }
+    }
+};
+//# sourceMappingURL=BrainEaters.js.map
---
+++ src/BrainEaters/wwwroot/js/BrainEaters.ts
@@ -0,0 +1,106 @@
+﻿window.onload = () => {
+    const spriteSize = 32;
+    const canvas = document.getElementById("gameSpace") as HTMLCanvasElement;
+    // ReSharper disable once AssignedValueIsNeverUsed
+    const context = canvas.getContext("2d");
+    const canvasWidth = canvas.offsetWidth;
+    const canvasHeight = canvas.offsetHeight;
+    const numCol = Math.floor(canvasWidth / spriteSize); //20
+    const numRow = Math.floor(canvasHeight / spriteSize); //15
+
+    //console.log(numCol);
+    //console.log(numRow);
+
+    const gameArr = []; //why const?
+    for (let i = 0; i < numRow; i++) {
+        gameArr[i] = [];
+    }
+
+    class GameObj {
+        gameObjImg: HTMLImageElement;
+
+        constructor(protected posRow: number,
+            protected posCol: number,
+            protected isSolid: boolean,
+            src: string = "../images/bg_tile.png") {
+            this.gameObjImg = new Image();
+            this.gameObjImg.onload = () => {
+                context.drawImage(this.gameObjImg, posCol * spriteSize, posRow * spriteSize, spriteSize, spriteSize);
+            };
+            this.gameObjImg.src = src;
+        }
+    }
+
+    class Player extends GameObj {
+
+        constructor() { super(1, (Math.floor(Math.random() * (numCol - 2) + 1)), true, "../images/plSprite.png"); }
+
+        mov() {}
+    }
+
+    class Zombie extends GameObj {
+
+        constructor(varRow) {
+            super(varRow, (Math.floor(Math.random() * (numCol - 2) + 1)), true, "../images/zomSprite.png");
+        }
+
+        mov() {
+            // ReSharper disable TsNotResolved
+            if (true /*changelater*/) {
+                this.posCol = Math.floor(Math.random() + 1);
+            }
+
+            this.posRow = Math.floor(Math.random() + 1);
+            // ReSharper restore TsNotResolved
+        }
+
+    }
+
+    populateGame();
+    console.log(gameArr);
+
+    // ReSharper disable VariableCanBeMadeConst
+    // ReSharper disable UnusedLocals
+    let player = new Player();
+
+
+    let zomRow: number;
+    do {
+        zomRow = Math.floor(Math.random() * (numRow - 6) + 5);
+    } while (zomRow % 2 === 0)
+    let zombie0 = new Zombie(zomRow);
+    do {
+        zomRow = Math.floor(Math.random() * (numRow - 6) + 5);
+    } while (zomRow % 2 === 0)
+    let zombie1 = new Zombie(zomRow);
+
+    // ReSharper restore UnusedLocals
+    // ReSharper restore VariableCanBeMadeConst
+
+    function populateGame() {
+        for (let row = 0; row < numRow; row++) {
+            const numEmptyMax = Math.floor(Math.random() * 2 + 4);
+            //console.log(numEmptyMax);
+            // ReSharper disable once VariableCanBeMovedToInnerBlock
+            let numEmpty = 0; //why move inside?
+            for (let col = 0; col < numCol; col++) {
+                //gameArr[row][col] = 0;
+                if (row === 0 || row === numRow - 1) {
+                    gameArr[row][col] = new GameObj(row, col, true);
+                } else if (col === 0 || col === numCol - 1) {
+                    gameArr[row][col] = new GameObj(row, col, true);
+                } else if (row % 2 !== 0) {
+                    gameArr[row][col] = -9;
+                } else {
+                    const coin = Math.floor(Math.random() * 10);
+                    if (coin <= 2 && numEmpty < numEmptyMax) {
+                        gameArr[row][col] = -9;
+                        ++numEmpty;
+                    } else {
+                        gameArr[row][col] = new GameObj(row, col, true);
+                    }
+                }
+            }
+        }
+    }
+}
